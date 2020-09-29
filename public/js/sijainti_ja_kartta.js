ee9989b3aa86fb2494f91782af1cf58dbc37379b
mirozzi <mirounnaslahti@gmail.com>
2020-09-29 19:46:36 +0300

    kartta

--- public/js/sijainti_ja_kartta.js
+++ public/js/sijainti_ja_kartta.js
@@ -18,9 +18,9 @@ if ("geolocation" in navigator) {
     document.getElementById("pituusasteet").innerHTML = latitude;
     document.getElementById("leveysasteet").innerHTML = longitude;
 
-    kartta = L.map('kartta').setWiew([latitude, longitude], 13);
+    kartta = L.map('kartta').setView([latitude, longitude], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }).addTo(kartta);
 
